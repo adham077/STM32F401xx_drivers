2ab25b5d3380a595808d70b82aad8c46229cea4a
adham077 <adham.walaa@gmail.com>
2025-01-27 17:18:53 +0200

    updates

--- COTS/MCAL/GPIO/build/CMakeFiles/GPIO.dir/src/GPIO_CFG.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/GPIO.dir/src/GPIO_CFG.c.obj: \
- C:\ITI\ Intake-45\ 9\ Months\github_repos\STM32F401xx_drivers\COTS\MCAL\GPIO\src\GPIO_CFG.c \
- C:/ITIINT~1/GITHUB~1/STM32F~1/COTS/MCAL/GPIO/inc/GPIO_CFG.h \
- C:/ITIINT~1/GITHUB~1/STM32F~1/COTS/STD_LIB/std_types.h
